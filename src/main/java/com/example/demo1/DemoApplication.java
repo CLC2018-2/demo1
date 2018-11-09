dd0797368372a864cc7a2cc1efcaa1d4869a2a39
Christoph Lukas <lukas@pingworks.de>
2018-11-09 16:47:07 +0100

    F-1 added comment

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Feature 1
 @SpringBootApplication
 public class DemoApplication {
 
